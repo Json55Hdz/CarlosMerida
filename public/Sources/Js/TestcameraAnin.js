0df804e1ac8b5ec7ece1aa226892769712557d95
JSON <jeisondh55@gmail.com>
2021-07-01 11:57:24 -0500

    Camera up pos

--- public/Sources/Js/TestcameraAnin.js
+++ public/Sources/Js/TestcameraAnin.js
@@ -16,7 +16,7 @@ AFRAME.registerComponent('cubito', {
         document.querySelector('#cubitosergios').addEventListener('click', function (evt) {
             if (!boolISetted){
                 tlCameraRot.paused(true);
-                MoveCamera(100,-100,300,0,-0.8,0,5,0,0.5,0,true);
+                MoveCamera(-70,-80.000,302.000,0,-0.8,0,5,0,0.5,0,true);
             }else{
                 console.log("ya esta seteado");
                 MoveCamera(0,150,1000,-0.349066,0,0,5,0,0,0,false);
